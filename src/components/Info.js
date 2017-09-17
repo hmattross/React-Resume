71180e95d792cf540a49ccddc8d942499a44b21b
hmattross <hmattross@gmail.com>
2017-09-17 11:39:51 -0500

    Changing to work on AWS
    Moved files and added package.json

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,13 +6,13 @@ class Info extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-4">
-                        <h5>hmattross@gmail.com</h5>
+                        <h5><b>hmattross@gmail.com</b></h5>
                     </div>
                     <div className="col-sm-4">
-                        <h5>832-277-6424</h5>
+                        <h5><b>832-277-6424</b></h5>
                     </div>
                     <div className="col-sm-4">
-                        <h5>19939 Chasewood Park, APT 5309, Houston, TX 77070</h5>
+                        <h5><b>19939 Chasewood Park, APT 5309, Houston, TX 77070</b></h5>
                     </div>
                 </div>
             </div>
